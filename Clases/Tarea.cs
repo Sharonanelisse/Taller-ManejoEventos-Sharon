aeb101a852cfc6095fc459d698491ed93b84926f
Sharon <168245948+Sharonanelisse@users.noreply.github.com>
2024-09-14 16:26:00 -0600

    Add files via upload
    archivos pendientes

---
+++ Clases/Tarea.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioAgo17.Clases
+{
+    internal class Tarea
+    {
+        public string nombre { get; set; }
+        public readonly DateTime fecha;
+        public string? estado { get; set; }
+
+        public Tarea(string nombre, DateTime fecha, string? estado)
+        {
+            this.nombre = nombre;
+            this.fecha = DateTime.Now;
+            this.estado = estado;
+        }
+
+    }
+}
